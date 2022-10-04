fc49487c800f5b4e4fff00ba3743477457c7753e
gitteddyhub <87045801+gitteddyhub@users.noreply.github.com>
2022-10-04 14:28:20 +0900

    Create pdarrange.py

---
+++ pdarrange.py
@@ -0,0 +1,58 @@
+rev
+tag no
+service
+instrument type
+line size
+line spec
+fluid name
+fluid status
+operating pressure-max
+operating pressure-nor
+operating pressure-min
+operating temperature-max
+operating temperature-nor
+operating temperature-min
+design pressure-int
+design pressure-ext
+design temperature-hot
+design temperature-cold
+flow rate-max
+flow rate-nor
+flow rate-min
+# 유량 단위 pd 에 없음
+max allow dp
+density
+viscosyty
+vapor m.w
+cp/cv
+z
+critical pressure
+vapor pressure
+flash percent
+shut-off dp
+faulure action
+=Level
+fluid name - lighter
+fluid status - lighter
+density-lighter
+viscosity-lighter
+=Temp
+# Liquid 와 Vapor 의 유량, 밀도, 점도가 다른 칸에 분리되어 기입되어있으므로 합쳐달라고 요구할 것
+=PSV
+out line size
+out line spec
+required capacity vapor (kg/h)
+required capacity liquid (kg/h)
+code
+sizing basis
+pressure nor # 앞의 pressure 와 통일 여부
+pressure set
+over pressure percent
+temperature nor # 앞의 temp 와 통일 여부
+temperature discharge
+superimposed pressure
+built-up back pressure
+viscosity-vapor # 앞의 점도와 통일여부
+viscosity-liquid # 앞의 점도와 통일여부
+
+
